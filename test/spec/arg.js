89a697482e5e038a7e7a4430a6e0102700cca75e
Mat Ryer <mat@stretchr.com>
2013-09-27 11:07:37 -0600

    added test framework

---
+++ test/spec/arg.js
@@ -0,0 +1,13 @@
+describe("Arg", function(){
+
+  it("Arg namespace should be defined", function(){
+    expect(Arg).toBeDefined();
+  });
+
+  it("Should be able to process a URL string into an object", function(){
+
+
+
+  });
+
+});
